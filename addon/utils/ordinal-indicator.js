5f19d038e97d303aa6fef1c37524af493bdd2ccf
Romina Vargas <rsocci@me.com>
2016-05-20 14:12:25 -0400

    Add ember suave

--- addon/utils/ordinal-indicator.js
+++ addon/utils/ordinal-indicator.js
@@ -1,6 +1,6 @@
 const { floor } = Math;
 
-export default function (number) {
+export default function(number) {
   if (floor(number / 10) === 1) {
     return 'th';
   } else if (number % 10 === 1) {
